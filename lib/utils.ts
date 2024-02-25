c77292c1ede606b6e6adf41bce79dadd74aa01b7
Sammwy <sammwy.dev@gmail.com>
2024-02-25 08:37:15 -0300

    fix: 🤞 geturl utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,3 @@
 export function getURL() {
-  return (
-    process.env.NEXT_PUBLIC_VERCEL_URL ||
-    process.env.NEXT_PUBLIC_BASE_URL ||
-    'http://localhost:3000'
-  );
+  return process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 }
