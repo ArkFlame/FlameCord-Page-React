7d9c15c555642cb0177987fb9c3b94981ee5a7c4
linsaftw <linsaftw@users.noreply.github.com>
2024-02-26 22:25:14 -0300

    update discount

--- config/product.ts
+++ config/product.ts
@@ -1,5 +1,5 @@
 // Set empty, null or undefined to disable this feature.
-export const DISCOUNT_CODE: string = 'uwu';
+export const DISCOUNT_CODE: string = '152552-WEB';
 
 export const PRODUCT_LINK: string =
   'https://builtbybit.com/resources/flamecord-ultimate-antibot-solution.13492/';
