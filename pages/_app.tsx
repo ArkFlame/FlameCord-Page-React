f192e3d2a336cbade755ba0a5a0ddf1df4ecadf5
linsaftw <linsaftw@users.noreply.github.com>
2024-04-05 21:12:19 -0300

    seo improved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,12 +16,24 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Head>
-        <title>FlameCord</title>
+        <title>FlameCord: Enhance Your Minecraft Server Performance</title>
         <meta
           name="description"
-          content="Enhance your Minecraft server with FlameCord: advanced security, top performance, seamless compatibility. Elevate your experience!"
+          content="Optimize your Minecraft server with FlameCord's advanced security, top performance, and seamless compatibility. Elevate your gaming experience today!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow"/>
+        <meta name="language" content="English"/>
+        <meta name="author" content="ArkFlame Development"/>
+        <meta name="keywords" content="FlameCord, Minecraft server, optimization, performance, security, compatibility" />
+
+        <meta property="og:title" content="FlameCord: Enhance Your Minecraft Server Performance"/>
+        <meta property="og:description" content="Optimize your Minecraft server with FlameCord's advanced security, top performance, and seamless compatibility. Elevate your gaming experience today!"/>
+        <meta property="og:image" content="https://www.flamecord.com/banner.png"/>
+        <meta property="og:url" content="https://www.flamecord.com"/>
+        <meta property="og:type" content="website"/>
+
+        <link rel="canonical" href="https://www.flamecord.com" />
         <link rel="icon" href="/flame.png" />
       </Head>
 
